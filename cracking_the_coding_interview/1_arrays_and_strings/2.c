49973266445e927a7b53fb06c88af8f252651a95
dpqkdqkj <dpqkdqkj@ro.ru>
2022-05-11 06:07:28 +0300

    Add 2 and 3 solutions from laptop

---
+++ cracking_the_coding_interview/1_arrays_and_strings/2.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>  /* type bool */
+#include <stdlib.h>  /* qsort */
+
+
+bool isPermutation_1(const char *first, const char *second)
+{
+    size_t len_first, len_second;
+    len_first = strlen(first);
+    len_second = strlen(second);
+
+    if (len_first != len_second) return 0;
+
+    size_t letter_counts[128];
+    for (size_t i = 0; i < len_first; ++i) {
+        letter_counts[first[i]]++;
+    }
+    for (size_t i = 0; i < len_second; ++i) {
+        letter_counts[second[i]]--;
+        if (letter_counts[second[i]] < 0) return 0;
+    }
+    return 1;
+}
+
+int cmpfunc(const void *left, const void *right);
+char *sortedString(const char *str);
+
+bool isPermutation_2(const char *first, const char *second)
+{
+    if (strlen(first) != strlen(second)) return 0;
+
+    char *first_sorted = sortedString(first);
+    char *second_sorted = sortedString(second);
+
+    int res_strcmp = strcmp(first_sorted, second_sorted);
+
+    free(first_sorted);
+    free(second_sorted);
+
+    return res_strcmp ? 0 : 1;
+}
+
+int main()
+{
+    char first[] = "abc";
+    char second[] = "cab";
+
+    if (isPermutation_1(first, second)) {
+        printf("First solution: YES!\n");
+    } else {
+        printf("First solution: NO.\n");
+    }
+
+    if (isPermutation_2(first, second)) {
+        printf("Second solution: YES!\n");
+    } else {
+        printf("Second solution: NO..\n");
+    }
+
+    return 0;
+}
+
+int cmpfunc(const void *left, const void *right)
+{
+    return *(char *)left - *(char *)right;
+}
+
+char *sortedString(const char *str)
+{
+    /* ALLOCATES MEMORY! */
+    char *sorted_str = strdup(str);
+    qsort(sorted_str, strlen(str), sizeof(char), cmpfunc);
+    return sorted_str;
+}
+
---
+++ cracking_the_coding_interview/1_arrays_and_strings/4.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+void initArray(size_t *array, size_t len_array);
+
+bool isPermutationOfPalindrome(char *str)
+{
+    size_t count_letters[128];
+    initArray(count_letters, 128);
+
+    for (size_t i = 0; i < strlen(str); ++i) {
+        if (str[i] == ' ') continue;
+        count_letters[str[i]]++;
+    }
+
+    bool oddFound = 0;
+    for (size_t i = 0; i < 128; ++i) {
+        if (count_letters[i] == 0) continue;
+        if (count_letters[i] % 2) {
+            if (oddFound) return 0;
+            oddFound = 1;
+        }
+    }
+    return 1;
+}
+
+
+int main()
+{
+    char test[] = "bclcb";
+    if (isPermutationOfPalindrome(test)) {
+        printf("YES.\n");
+    } else {
+        printf("NO.\n");
+    }
+    return 0;
+}
+
+void initArray(size_t *array, size_t len_array)
+{
+    memset(array, 0, len_array * sizeof(size_t));  /* initialize array by zeroes */
+}
+
