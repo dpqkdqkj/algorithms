53162828c979c54225e315290c47250cab48c367
dpqkdqkj <dpqkdqkj@ro.ru>
2022-05-12 19:51:21 +0300

    Merge solutions for 4 question in one file.

--- cracking_the_coding_interview/1_arrays_and_strings/2.c
+++ cracking_the_coding_interview/1_arrays_and_strings/2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stddef.h>
 #include <string.h>
 #include <stdbool.h>  /* type bool */
 #include <stdlib.h>  /* qsort */
--- cracking_the_coding_interview/1_arrays_and_strings/4.c
+++ cracking_the_coding_interview/1_arrays_and_strings/4.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <stddef.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 
+/* 
+ * ---- ---- ---- Solution 1 ---- ---- ----
+ */
 void initArray(size_t *array, size_t len_array);
 
-bool isPermutationOfPalindrome(char *str)
+bool isPermutationOfPalindrome_1(const char *str)
 {
     size_t count_letters[128];
     initArray(count_letters, 128);
@@ -26,20 +31,78 @@ bool isPermutationOfPalindrome(char *str)
     return true;
 }
 
+/* 
+ * ---- ---- ---- Solution 2 ---- ---- ----
+ */
+int getCharNumber(char c);
+int createBitVector(const char *str);
+void toggleBitVector(int *bit_vector, int index);
+int isOnlyOneBitSet(int bit_vector);
+
+bool isPermutationOfPalindrome_2(const char *str)
+{
+    int bit_vector = createBitVector(str);
+    return bit_vector == 0 || isOnlyOneBitSet(bit_vector);
+}
+
+/* 
+ * ---- ---- ---- Tests ---- ---- ----
+ */
+bool test(bool (*f)(const char*));
 
 int main()
 {
-    char test[] = "bclcb";
-    if (isPermutationOfPalindrome(test)) {
-        printf("YES.\n");
-    } else {
-        printf("NO.\n");
-    }
+    printf("%s\n", test(isPermutationOfPalindrome_1) ? "OK." : "FAIL.");
+    printf("%s\n", test(isPermutationOfPalindrome_2) ? "OK." : "FAIL.");
     return 0;
+}
+
+
+bool test(bool (*f)(const char*)) 
+{
+    char test_true[] = "abab";
+    char test_false[] = "abad";
+    return (f(test_true) && ~f(test_false));
 }
 
 void initArray(size_t *array, size_t len_array)
 {
-    memset(array, 0, len_array * sizeof(size_t));  /* initialize array by zeroes */
+    memset(array, 0, len_array * sizeof(size_t));
+}
+
+int getCharNumber(char c)
+{
+    if (('a' <= c) && (c <= 'z')) {
+        return c - 'a';
+    }
+    return -1;
+}
+
+void toggleBitVector(int *bit_vector, int index)
+{
+    if (index < 0) return;
+
+    int mask = 1 << index;
+
+    if ((*bit_vector & mask) == 0) {
+        *bit_vector |= mask;
+    } else {
+        *bit_vector &= ~mask;
+    }
+}
+
+int createBitVector(const char *str) {
+    int bit_vector = 0;
+    int *ptr_bv = &bit_vector;
+    
+    for (int i = 0; i < strlen(str); ++i) {
+        toggleBitVector(ptr_bv, getCharNumber(str[i]));
+    }
+    return bit_vector;
+}
+
+int isOnlyOneBitSet(int bit_vector)
+{
+    return ((bit_vector - 1) & bit_vector) == 0;
 }
 
