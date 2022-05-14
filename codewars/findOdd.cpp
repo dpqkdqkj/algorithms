1aed5765b7ede6b28564ce988601150ae8faf32c
dpqkdqkj <dpqkdqkj@ro.ru>
2022-05-14 22:58:10 +0300

    remove main

--- codewars/findOdd.cpp
+++ codewars/findOdd.cpp
@@ -15,25 +15,3 @@ int findOdd(const std::vector<int> &numbers)
     return 0;
 }
 
-
-int main()
-{
-    std::vector<int> numbers;
-    numbers.push_back(5);
-    numbers.push_back(5);
-    numbers.push_back(1);
-    numbers.push_back(1);
-    numbers.push_back(1);
-    numbers.push_back(1);
-    numbers.push_back(3);
-    numbers.push_back(3);
-    numbers.push_back(2);
-    numbers.push_back(2);
-    numbers.push_back(12121);
-
-
-    printf("%d\n", findOdd(numbers));
-    printf("%X\n", 0x010101 ^ 0x100001);
-    return 0;
-}
-
