ed0749cda0db3425a2540f6e18f95c25b117b922
dpqkdqkj <dpqkdqkj@ro.ru>
2022-06-05 03:30:21 +0300

    Retab

--- myds/Array.hpp
+++ myds/Array.hpp
@@ -8,63 +8,63 @@ namespace myds {
 template<class T>
 class Array {
 protected:
-	T *data;
+    T *data;
 public:
-	int length;
-	Array();
-	Array(int len);
-	Array(int len, T init);
+    int length;
+    Array();
+    Array(int len);
+    Array(int len, T init);
     Array(const Array<T>& a);
 
-	const Array<T>& operator=(const Array<T>& other) {
-		if (data != nullptr) delete [] data;
+    const Array<T>& operator=(const Array<T>& other) {
+        if (data != nullptr) delete [] data;
         length = other.length;
         data = new T[length];
 
         for (int i = 0; i < length; ++i)
             data[i] = other.data[i];
 
-		return *this;
-	}
+        return *this;
+    }
     /* If we don't care about semantic traditions
      * we can type the following:
      *
-	void operator=(const Array<T>& other) {
-		if (data != nullptr) delete [] data;
+    void operator=(const Array<T>& other) {
+        if (data != nullptr) delete [] data;
 
         length = other.length;
         data = new T[length];
         for (int i = 0; i < length; ++i)
             data[i] = other.data[i];
-	}
+    }
     */
 
-	T& operator[](int i) {
-		assert(i >= 0 && i < length);
-		return data[i];
-	}
+    T& operator[](int i) {
+        assert(i >= 0 && i < length);
+        return data[i];
+    }
 
-	virtual ~Array();
+    virtual ~Array();
 };
 
 template<class T>
 Array<T>::Array() {
-	length = 0;
-	data = nullptr;
+    length = 0;
+    data = nullptr;
 }
 
 template<class T>
 Array<T>::Array(int len) {
-	length = len;
-	data = new T[length];
+    length = len;
+    data = new T[length];
 }
 
 template<class T>
 Array<T>::Array(int len, T init) {
-	length = len;
-	data = new T[length];
-	for (int i = 0; i < length; ++i)
-		data[i] = init;
+    length = len;
+    data = new T[length];
+    for (int i = 0; i < length; ++i)
+        data[i] = init;
 }
     
 template<class T>
@@ -74,7 +74,7 @@ Array<T>::Array(const Array<T>& a) {
 
 template<class T>
 Array<T>::~Array() {
-	if (data != nullptr) delete[] data;
+    if (data != nullptr) delete[] data;
 }
 
 } /* namespace ods */
