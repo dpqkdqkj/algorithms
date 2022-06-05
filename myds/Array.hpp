abe0bc6809fbc5ca467dd7ae137c31fc40da1ded
dpqkdqkj <dpqkdqkj@ro.ru>
2022-06-05 03:26:37 +0300

    Array total code change

--- myds/Array.hpp
+++ myds/Array.hpp
@@ -1,32 +1,82 @@
-#include <cassert>  // assert
+#ifndef ARRAY_H_
+#define ARRAY_H_
 
-template <class T>
+#include <assert.h>  // assert
+
+namespace myds {
+
+template<class T>
 class Array {
-    T *data;
-    int length;
+protected:
+	T *data;
 public:
-    /* Init construction */
-    Array(int len)
-    {
-        length = len;
+	int length;
+	Array();
+	Array(int len);
+	Array(int len, T init);
+    Array(const Array<T>& a);
+
+	const Array<T>& operator=(const Array<T>& other) {
+		if (data != nullptr) delete [] data;
+        length = other.length;
         data = new T[length];
-    };
 
-    T& operator[](int i)
-    {
-        assert(i >= 0 && i < length);
-        return data[i];
-    };
+        for (int i = 0; i < length; ++i)
+            data[i] = other.data[i];
 
-    Array<T>& operator=(Array<T> &b)
-    {
-        if (this->data != nullptr) delete[] this->data;
+		return *this;
+	}
+    /* If we don't care about semantic traditions
+     * we can type the following:
+     *
+	void operator=(const Array<T>& other) {
+		if (data != nullptr) delete [] data;
 
-        this->data = b.data;
-        b.data = nullptr;
-        this->length = b.length;
+        length = other.length;
+        data = new T[length];
+        for (int i = 0; i < length; ++i)
+            data[i] = other.data[i];
+	}
+    */
 
-        return *this;
-    };
+	T& operator[](int i) {
+		assert(i >= 0 && i < length);
+		return data[i];
+	}
+
+	virtual ~Array();
 };
+
+template<class T>
+Array<T>::Array() {
+	length = 0;
+	data = nullptr;
+}
+
+template<class T>
+Array<T>::Array(int len) {
+	length = len;
+	data = new T[length];
+}
+
+template<class T>
+Array<T>::Array(int len, T init) {
+	length = len;
+	data = new T[length];
+	for (int i = 0; i < length; ++i)
+		data[i] = init;
+}
+    
+template<class T>
+Array<T>::Array(const Array<T>& a) {
+    *this = a;
+}
+
+template<class T>
+Array<T>::~Array() {
+	if (data != nullptr) delete[] data;
+}
+
+} /* namespace ods */
+#endif /* ARRAY_H_ */
 
