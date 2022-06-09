c77c5eb872832b8237787932535d6a18a1feec57
dpqkdqkj <dpqkdqkj@ro.ru>
2022-06-09 23:23:54 +0300

    Fixed shift methods

--- myds/ArrayDeque.hpp
+++ myds/ArrayDeque.hpp
@@ -73,24 +73,47 @@ void ArrayDeque<T>::shift_right(size_t start_idx, size_t end_idx)
 template<class T>
 void ArrayDeque<T>::add(size_t i, T x)
 {
+    /*
+     *   0 1 2 3 4 5 6 7
+     * [ a b c d e f g h _ _ __ __ ]
+     *   0 1 2 3 4 5 6 7 8 9 10 11
+     *
+     *   first_idx = 0;
+     *   ad_N = 8;
+     *   ad_N / 2 = 4;
+     *   i from [0, 3] --> shift_left 
+     *   i from [4, inf] --> shift_right 
+     *
+     *   add(3, 'x') --> shift_left
+     *   1 2 3 4 5 6 7 8        0
+     * [ b c x d e f g h _ _ __ a ]
+     *   0 1 2 3 4 5 6 7 8 9 10 11
+     *
+     *   add(5, 'y') --> shift_right
+     *   1 2 3 4 5 6 7 8        0
+     * [ b c x d e f g h _ _ __ a ]
+     *   0 1 2 3 4 5 6 7 8 9 10 11
+     *
+     *   remove(6) --> shift_left
+     *   1 2 3 4 5 6 7          0
+     * [ b c x d e g h h _ _ __ a ]
+     *   0 1 2 3 4 5 6 7 8 9 10 11
+     *
+     *   add(6, z) --> shift_right
+     *   1 2 3 4 5 6 7 8        0
+     * [ b c x d e z g h _ _ __ a ]
+     *   0 1 2 3 4 5 6 7 8 9 10 11
+     */
     if (ad_N + 1 > array.size()) resize();
 
     if (i < ad_N / 2) // shift a[0],..,a[i-1] left one position
     {
         first_idx = (first_idx == 0) ? array.size() - 1 : first_idx - 1;
-        /*
-        for (size_t k = 0; k <= i - 1; ++k)
-            array[ (first_idx + k) % array.size() ] = array[ (first_idx + k + 1) % array.size() ];
-            */
-        //if (i != 0)
-        shift_left(0, i - 1);
+        if (i != 0)
+            shift_left(0, i - 1);
     }
     else // shift a[i],..,a[n-1] right one position
     {
-        /*
-        for (size_t k = ad_N; k > i; --k)
-            array[ (first_idx + k) % array.size() ] = array[ (first_idx + k - 1) % array.size() ];
-            */
         shift_right(i, ad_N);
     }
 
@@ -105,27 +128,17 @@ T ArrayDeque<T>::remove(size_t i)
 
     if (i < ad_N / 2) // shift a[0],..,[i-1] right one position
     {
-        /*
-        for (size_t k = i; k > 0; --k)
-            array[ (first_idx + k) % array.size() ] = array[ (first_idx + k - 1) % array.size() ];
-        */
-
         shift_right(0, i);
         first_idx = (first_idx + 1) % array.size();
     }
     else // shift a[i+1],..,a[n-1] left one position
     {
-        /*
-        for (size_t k = i; k < ad_N - 1; ++k)
-            array[ (first_idx + k) % array.size() ] = array[ (first_idx + k + 1) % array.size() ];
-        */
-        if (ad_N != 0)
-            shift_left(i, ad_N - 1);
-            //shift_left(i, ad_N);
+        if (ad_N > 2)
+            shift_left(i, ad_N - 2);
     }
 
     --ad_N;
-    //if (3 * ad_N < array.size()) resize();
+    if (3 * ad_N < array.size()) resize();
 
     return x;
 }
