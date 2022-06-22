968274167d15e6b846253ed4da77bcb185248ca4
dpqkdqkj <dpqkdqkj@ro.ru>
2022-06-22 23:06:52 +0300

    Solution with ternary operator

--- codewars/schoolPaperwork.cpp
+++ codewars/schoolPaperwork.cpp
@@ -8,11 +8,19 @@ int paperwork(int n, int m)
     return n * m;
 }
 
+int paperwork2(int n, int m)
+{
+    return (n < 0 || m < 0) ? 0 : n * m;
+}
+
 
 int main()
 {
     printf("%d\n", paperwork(5, 5));
     printf("%d\n", paperwork(-5, 5));
+
+    printf("%d\n", paperwork2(-5, 5));
+    printf("%d\n", paperwork2(-5, 5));
     return 0;
 }
 
