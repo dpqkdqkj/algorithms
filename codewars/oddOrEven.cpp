fe0dfdd8d0840c67189f0462f2719cebe5078d4c
dpqkdqkj <dpqkdqkj@ro.ru>
2022-07-05 07:01:51 +0300

    Remove check empty size the vector

--- codewars/oddOrEven.cpp
+++ codewars/oddOrEven.cpp
@@ -5,12 +5,8 @@
 
 std::string odd_or_even(const std::vector<int> &arr)
 {
-    size_t arr_len = arr.size();
-
-    if (arr_len == 0) return "even";
-
     int sum = 0;
-    for (size_t i = 0; i < arr_len; ++i)
+    for (size_t i = 0; i < arr.size(); ++i)
         sum += arr[i];
 
     return (sum % 2 == 0) ? "even" : "odd";
@@ -18,7 +14,7 @@ std::string odd_or_even(const std::vector<int> &arr)
 
 int main (int argc, char *argv[])
 {
-    printf("%s\n", odd_or_even({0,-1,-5}).c_str());
+    printf("%s\n", odd_or_even({}).c_str());
     return 0;
 }
 
